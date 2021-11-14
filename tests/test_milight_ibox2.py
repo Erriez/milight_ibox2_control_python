191513be1a46ea4aa020e00895838648554a0e19
Erriez <Erriez@users.noreply.github.com>
2021-11-14 19:51:33 +0100

    Upgrade to Python3 with 2to3 tool

--- tests/test_milight_ibox2.py
+++ tests/test_milight_ibox2.py
@@ -17,20 +17,20 @@ class TestMilightIBox(unittest.TestCase):
         print("Testing connect/disconnect...")
 
         ibox = MilightIBox(ibox_ip, ibox_port=5987, sock_timeout=2, tx_retries=5, verbose=verbose)
-        self.assertEquals(ibox.is_connected(), False)
+        self.assertEqual(ibox.is_connected(), False)
         ibox.connect()
-        self.assertEquals(ibox.is_connected(), True)
+        self.assertEqual(ibox.is_connected(), True)
         ibox.disconnect()
-        self.assertEquals(ibox.is_connected(), False)
+        self.assertEqual(ibox.is_connected(), False)
 
     def test_timeout(self):
         print("Testing timeout...")
 
         ibox = MilightIBox(ibox_ip="192.168.0.1", ibox_port=5987, sock_timeout=2, tx_retries=5,
                            verbose=verbose)
-        self.assertEquals(ibox.is_connected(), False)
+        self.assertEqual(ibox.is_connected(), False)
         ibox.connect()
-        self.assertEquals(ibox.is_connected(), False)
+        self.assertEqual(ibox.is_connected(), False)
         ibox.disconnect()
 
     def test_light_on_off(self):
@@ -43,19 +43,19 @@ class TestMilightIBox(unittest.TestCase):
 
         ibox.send_light_off(0)
         print("Are all lights off? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_light_on(zone)
         print("Are all lights on? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_light_off(zone)
         print("Are all lights off? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_night_light_on(zone)
         print("Are all lights in night light? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.disconnect()
 
@@ -69,23 +69,23 @@ class TestMilightIBox(unittest.TestCase):
 
         ibox.send_brightness(1, zone)
         print("Is brightness of all lights 1%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_brightness(25, zone)
         print("Is brightness of all lights 25%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_brightness(100, zone)
         print("Is brightness of all lights 100%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_brightness(75, zone)
         print("Is brightness of all lights 75%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_brightness(50, zone)
         print("Is brightness of all lights 50%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.disconnect()
 
@@ -100,19 +100,19 @@ class TestMilightIBox(unittest.TestCase):
 
         ibox.send_color_temperature(2700, zone)
         print("Is color temperature of all lights 2700K? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_color_temperature(6500, zone)
         print("Is color temperature of all lights 6500K? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_color_temperature(5500, zone)
         print("Is color temperature of all lights 5500K? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_color_temperature(4000, zone)
         print("Is color temperature of all lights 4000K? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.disconnect()
 
@@ -127,27 +127,27 @@ class TestMilightIBox(unittest.TestCase):
 
         ibox.send_rgb_color(0x10, zone)
         print("Is RGB color of all lights red? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_rgb_color(0x60, zone)
         print("Is RGB color of all lights green? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_rgb_color(0xb0, zone)
         print("Is RGB color of all lights blue? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_saturation(100, zone)
         print("Is saturation of all lights 100%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_saturation(50, zone)
         print("Is saturation of all lights 50%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_saturation(0, zone)
         print("Is saturation of all lights 0%? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.disconnect()
 
@@ -161,11 +161,11 @@ class TestMilightIBox(unittest.TestCase):
 
         ibox.send_mode(1, zone)
         print("Is mode 1? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         ibox.send_mode(9, zone)
         print("Is mode 9? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
     def test_zones(self):
         print("Testing zones...")
@@ -175,15 +175,15 @@ class TestMilightIBox(unittest.TestCase):
 
         ibox.send_light_off(0)
         print("Are all lights off? y/[n]")
-        self.assertEquals(raw_input(), "y")
+        self.assertEqual(input(), "y")
 
         for _zone in range(1, 5):
             ibox.send_light_on(_zone)
             ibox.send_white_light_on(_zone)
             ibox.send_brightness(75, _zone)
             ibox.send_color_temperature(4000, _zone)
-            print("Is light %d on? y/[n]" % _zone)
-            self.assertEquals(raw_input(), "y")
+            print(("Is light %d on? y/[n]" % _zone))
+            self.assertEqual(input(), "y")
 
         ibox.disconnect()
 
